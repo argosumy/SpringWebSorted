f7b9e0d2a2aba19291685465c2774e37f7d594b5
Valeriy Lozinskiy <argosumy@gmail.com>
2021-02-05 23:36:40 +0200

    validation

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,5 +1,4 @@
 package com.example.demo.controllers;
-
 import com.example.demo.models.Numbers;
 import com.example.demo.services.NumbersService;
 import com.fasterxml.jackson.core.JsonParser;
@@ -7,45 +6,54 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.servlet.ModelAndView;
+import org.thymeleaf.util.StringUtils;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+@Validated
 @Controller
 public class MainController {
     private NumbersService numbersService;
+    private Numbers numbers;
+
     @Autowired
-    public MainController(NumbersService numbersService) {
+    public MainController(NumbersService numbersService, Numbers numbers) {
+        this.numbers = numbers;
         this.numbersService = numbersService;
     }
 
     @GetMapping(value = "/")
-    public String getMainMenu(){
+    public String getMainMenu(Model model){
+        model.addAttribute("numbers", numbers);
         return "mainPage";
     }
 
     @PostMapping(value = "/arrayNumber")
-    public String getArrayNonSort(Model model,
-                                  @RequestParam(value = "quantity")int quantity){
-        Numbers numbers = new Numbers();
-        numbers.setIntegerListMap(numbersService.makeNewMapNonSort(quantity));
-//        numbers.setQuantity(quantity);
-        model.addAttribute("numbers",numbers);
+    public String getArrayNonSort(@ModelAttribute("numbers")@Valid Numbers numbers,
+                                  BindingResult bindingResult,Model model){
+        this.numbers = numbers;
+        this.numbers.setIntegerListMap(numbersService.makeNewMapNonSort(numbers.getQuantity()));
+        model.addAttribute("numbers",this.numbers);
         model.addAttribute("action","up");
         return "resultPage";
     }
 
     @GetMapping(value = "/arrayNumber/{quantity}")
-    public String getArrayNonSSort( Model model,
-                                    @PathVariable("quantity")int quantity){
-        Numbers numbers = new Numbers();
-        numbers.setIntegerListMap(numbersService.makeNewMapNonSort(quantity));
-//        numbers.setQuantity(quantity);
-        model.addAttribute("numbers",numbers);
+    public String getArrayNonSort( Model model,
+                                    @PathVariable("quantity")@Max(value = 30, message = "Pick a number before 30")int quantity){
+        this.numbers = new Numbers();
+        this.numbers.setIntegerListMap(numbersService.makeNewMapNonSort(quantity));
+        model.addAttribute("numbers",this.numbers);
         model.addAttribute("action","up");
         return "resultPage";
     }
@@ -57,21 +65,33 @@ public class MainController {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
         Map<Integer,List<Integer>> mapList = mapper.readValue(mapJson,new TypeReference<HashMap<Integer,List<Integer>>>(){});
-        List<Integer> list = numbersService.parseMapToList(mapList);
-        list = numbersService.sortedList(list,action);
-        Numbers numbers = new Numbers();
-        mapList = numbersService.parseListToMap(list);
-        numbers.setIntegerListMap(mapList);
+        model.addAttribute("numbers",numbersService.sortedList(mapList,action));
         if(action.equals("up")){
             model.addAttribute("action","down");
         }
         else{
             model.addAttribute("action","up");
         }
-        model.addAttribute("numbers",numbers);
         return "resultPage";
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ModelAndView handleValidationExceptions(ConstraintViolationException e){
+        ModelAndView modelAndView = new ModelAndView();
+        String error = StringUtils.substringBefore(e.getLocalizedMessage(),":");
+        if(error.equals("getArrayNonSort.numbers.quantity")){
+            modelAndView.setViewName("mainPage");
+            modelAndView.addObject("error", StringUtils.substringAfter(e.getMessage(),":"));
+            modelAndView.addObject("numbers",new Numbers());
+        }else {
+            modelAndView.setViewName("resultPage");
+            modelAndView.addObject("numbers",numbers);
+            modelAndView.addObject("action","up");
+            modelAndView.addObject("error", StringUtils.substringAfter(e.getMessage(),":"));
+        }
+        return modelAndView;
+    }
 
 
 }
--- src/main/java/com/example/demo/services/NumbersService.java
+++ src/main/java/com/example/demo/services/NumbersService.java
@@ -1,69 +1,12 @@
 package com.example.demo.services;
 
 import com.example.demo.models.Numbers;
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Map;
 
-@Component
-@Data
-public class NumbersService {
-    Numbers numbers;
+public interface NumbersService {
 
-    public NumbersService() {
-    }
-
-    @Autowired
-    public NumbersService(Numbers numbers) {
-        this.numbers = numbers;
-    }
-
-    public Map<Integer,List<Integer>> makeNewMapNonSort(int quantity){
-        Random random = new Random();
-        List<Integer> listNonSort = random.ints(1,1001).
-                                    distinct().limit(quantity).boxed().collect(Collectors.toList());
-        return parseListToMap(listNonSort);
-    }
-
-    public List<Integer> sortedList(List<Integer> list, String action){
-        if(action.equals("up")){
-            Collections.sort(list);
-        }
-        else{
-            Collections.reverse(list);
-        }
-        return list;
-    }
-    //Map <key,Value<List(max 10 numbers)>>
-    public Map<Integer, List<Integer>> parseListToMap(List<Integer> listParse){
-        Map<Integer,List<Integer>> mapList = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-        int keyMap = 1;
-        int marker;
-        for(int i = 0; i < listParse.size(); i++){
-            list.add(listParse.get(i));
-            marker = i + 1;
-            if(((marker % 10) == 0) || (marker == listParse.size())){
-                   mapList.put(keyMap,list);
-                   keyMap++;
-                   list = new ArrayList<>();
-            }
-        }
-        return mapList;
-    }
-
-    public List<Integer> parseMapToList(Map<Integer, List<Integer>> mapList){
-        List<Integer> list = new ArrayList<>();
-        for (int key:mapList.keySet()) {
-            list.addAll(mapList.get(key));
-        }
-        return list;
-    }
-
-
-
-
+   Map<Integer, List<Integer>> makeNewMapNonSort(int quantity);
+   Numbers sortedList(Map<Integer,List<Integer>> map, String action);
 }
